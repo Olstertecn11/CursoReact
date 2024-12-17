0944130f41ba9c7cb40b5c8f43f6d4478093cc25
Olstertecn11 <otzunund@miumg.edu.gt>
2024-12-17 01:58:46 -0600

    Login con alerta finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 import { useState } from "react";
 
+// Los componentes reciben parametros (props)
+const Alerta = ({ title, message, type, show = false }) => {
+  const color = type == 'success' ? 'green' : 'red';
+
+  const display_type = show ? 'block' : 'none';
+
+  return (
+    <div style={{ background: color, display: display_type }}>
+      <p> <b>{title}</b> {message} </p>
+    </div>
+  )
+}
+
+
 const Login = () => {
 
+  // Variables
   const empty_user = { username: '', password: '' };
+  const empty_alert = { title: '', message: '', type: '', show: false };
+  const success_alert = {
+    title: 'Éxito',
+    message: 'Bienvenido al sistema', type: 'success', show: true
+  }
+  const error_alert = {
+    title: 'Error',
+    message: 'Credenciales incorrectas', type: 'error', show: true
+  }
+
+
+
+  // States
   const [user, setUser] = useState(empty_user);
+  const [alerta, setAlert] = useState(empty_alert)
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -12,24 +41,37 @@ const Login = () => {
     setUser(nuevo_user);
   }
 
+  const clearAlert = () => {
+    setTimeout(() => {
+      setAlert(empty_alert);
+    }, 3000)
+  }
+
 
   const auth = () => {
-    alert(user.username);
-    // Terminar el ejemplo verificando que username y password sean iguales a 'admin'
+    if (user.username == 'admin' && user.password == 'admin') {
+      setAlert(success_alert);
+      clearAlert();
+      return;
+    }
+    setAlert(error_alert);
+    clearAlert();
   }
 
   return (
-    <div>
-      <h2>Login</h2>
-      <label htmlFor="">Usuario</label>
-      <br />
-      <input type="text" placeholder="Ingrese su usuario" name="username" onChange={handleChange} value={user.username} />
-      <br />
-      <label htmlFor="">Contraseña</label>
-      <br />
-      <input type="password" onChange={handleChange} value={user.password} name="password" />
-      <br />
-      <button onClick={auth}>Entrar</button>
+    <div className="container">
+      <Alerta title={alerta.title} message={alerta.message} type={alerta.type} show={alerta.show} />
+      <div className="row mt-4">
+        <div className="col-md-5 mx-auto">
+          <div className="card card-body p-4">
+            <label htmlFor="">Usuario</label>
+            <input className="form-control" type="text" placeholder="Ingrese su usuario" name="username" onChange={handleChange} value={user.username} />
+            <label htmlFor="" className="mt-4">Contraseña</label>
+            <input className="form-control" type="password" onChange={handleChange} value={user.password} name="password" />
+            <button className="mt-4 btn btn-warning" onClick={auth}>Entrar</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
